23421a1ae40bc21e01c6d63d390b4dcc22d69518
Patrick Simmelbauer <psimmelbauer@slashwhy.de>
2024-08-31 14:48:02 +0200

    add initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [link, setLink] = useState<string | null>(null)
+
+  useEffect(() => {
+    fetch("/links.txt").then(async (res)=> {
+      const text = await res.text()
+      const links = text.split("\n").map(link => link.trim());
+      const next = (index: number) => {
+        const link = links[index % links.length]
+        console.log(`showing link ${link}`)
+        setLink(link)
+        setTimeout(() => next(index+1), 10000)
+      }
+     next(0);
+    })
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      link && <iframe
+        id="inlineFrameExample"
+        title="Inline Frame Example"
+        allow={`camera ${link}; microphone ${link}`}
+        src={link}>
+      </iframe>
   )
 }
 
